a5e7b2bdda209c42eb9bbfcfe3b6c2254376391f
Oleksandr Vlasov <drumlife182@gmail.com>
2024-03-16 21:20:14 +0100

    Clean up

--- main.go
+++ main.go
@@ -2,22 +2,8 @@ package main
 
 import (
 	"fmt"
-
-	inmemorycache "exmaple.com/home-work/home-work1/in-memory-cache"
 )
 
 func main() {
-	// Create cache
-	cache := inmemorycache.NewCache()
-
-	// Set new value
-	cache.Set("userId", 123)
-
-	// Get value by key
-	cache.Get("userId")
-
-	// Delete value by key
-	// cache.Delete("userId")
-
-	fmt.Println(cache)
+	fmt.Println("Hello Gophers")
 }
